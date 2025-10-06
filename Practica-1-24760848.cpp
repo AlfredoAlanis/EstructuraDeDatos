4e55be8bd6c1d6582938b1586bf2402c19145cd9
AlfredoAlanis <al24760848@ite.edu.mx>
2025-10-05 21:45:13 -0700

    ultima

--- Practica-1-24760848.cpp
+++ Practica-1-24760848.cpp
@@ -4,8 +4,7 @@ using namespace std;
 int main(){
 //variable para decidir el numero de datos que tendra la matriz en este caso 4x4
 int matriz[4][4] = {0};
-//esta para en que numero comenzara
-int Contador = 1;
+
 //ciclo para recorrer las filas de la matriz
 for(int i = 0; i < 4; i++){
     //esta otra parte para las columnas
