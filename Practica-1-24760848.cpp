a58114aa050c06166ecb56fdc1de32ac3c277c27
AlfredoAlanis <al24760848@ite.edu.mx>
2025-10-05 21:25:27 -0700

    prueba-1 para ver si se ejecuta correctamente

--- Practica-1-24760848.cpp
+++ Practica-1-24760848.cpp
@@ -1,3 +1,21 @@
-//pruebadecommitparagithubyvisual
-//pruebade commit02
-#include <iostream> veamos estoy haciendo pruebas mientras verifico la conexion con visual studio ya que me entretuve en otras cosas :3
+#include <iostream>
+using namespace std;
+
+int main(){
+//variable para decidir el numero de datos que tendra la matriz en este caso 4x4
+int matriz[4][4];
+//esta para en que numero comenzara
+int Contador = 1;
+//ciclo para recorrer las filas de la matriz
+for(int i = 0; i < 4; i++){
+    //esta otra parte para las columnas
+    for (int j = 0; j < 4; j++)
+    {
+        matriz[i][j] = Contador;
+        Contador++;
+        cout << matriz[i][j] << " ";
+    }
+    cout << endl;
+} 
+return 0;
+}
