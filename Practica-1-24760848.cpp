ab3b894ad0ac6de6fb55bb54524d2ffe4f91e30f
AlfredoAlanis <al24760848@ite.edu.mx>
2025-10-05 18:46:52 -0700

    prueba2

--- Practica-1-24760848.cpp
+++ Practica-1-24760848.cpp
@@ -1,3 +1,3 @@
 //pruebadecommitparagithubyvisual
 //pruebade commit02
-#include <iostream>
+#include <iostream> veamos estoy haciendo pruebas mientras verifico la conexion con visual studio ya que me entretuve en otras cosas :3
